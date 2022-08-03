e4c3d07cd93ff62fadbcda92fd999c3eb9b0199a
Burak Kahraman <burak.kahramann16@gmail.com>
2022-08-03 13:03:27 +0300

    icons changed

--- pages/spsf.js
+++ pages/spsf.js
@@ -7,7 +7,7 @@ import {useDropzone} from 'react-dropzone';
 import updateAction from "../_helpers/updateAction";
 import { useForm } from "react-hook-form";
 import { useStateMachine } from "little-state-machine";
-
+import Image from "next/image";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import styles from '../styles/spsf.module.scss';
@@ -128,13 +128,9 @@ export default function Spsf({mData}) {
                                 <div className={styles.accounttype+" w-full h-[150px] flex flex-row mr-3 mb-3 md:mb-0"}>
                                     <input {...register("accounttype")} defaultValue={state.accounttype} type="radio" className="btncheck" name="accounttype" value="agency" id="agency-01" autoComplete="off" />
                                     <label htmlFor="agency-01" className="w-full h-[150px] border rounded flex flex-col justify-center items-center transition ease-in">
-                                        <svg className="mb-3" xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 16.933 16.933">
-                                            <g transform="translate(0 -280.067)">
-                                                <path fill="none" stroke="#00003a" strokeLinecap="round" strokeLinejoin="round" strokeWidth=".529" d="M5.855718 280.59582c-1.1447391 0-2.0649578.91071-2.0649578 2.04356v13.83144h9.3518218v-13.83144c0-1.13285-.92022-2.04356-2.064977-2.04356zM2.8399138 285.41743H2.5941292c-1.1447431 0-2.06496273.91067-2.06496273 2.04352v9.00987H3.7907602"/>
-                                                <path fill="none" stroke="#00003a" strokeLinecap="round" strokeLinejoin="round" strokeWidth=".529" d="M14.136491 285.41743h.202722c1.144743 0 2.064963.91067 2.064963 2.04352v9.00987h-3.261594M2.0212258 289.19905H3.79076M2.0212258 291.23698H3.79076M5.5857346 286.89629H7.3552688M5.5857346 288.53332H7.3552688M9.5780732 286.89629H11.347608M9.5780732 288.53332H11.347608M13.142582 289.19905h1.769534M13.142582 291.23698h1.769534M7.7964278 292.25609c-.5170363 0-.9332764.41624-.9332764.93327v3.28146h3.2070396v-3.28146c0-.51703-.4162403-.93327-.9332766-.93327zM5.5857346 290.4042H7.3552688M9.5780732 290.4042H11.347608"/>
-                                                <path fill="none" stroke="#00003a" strokeWidth=".529" d="m 8.2938067,281.99055 c -0.32104,0 -0.57929,0.25825 -0.57929,0.57929 v 0.47698 h -0.47697 c -0.32105,0 -0.5793,0.25824 -0.5793,0.57929 v 0.34571 c 0,0.32105 0.25825,0.5793 0.5793,0.5793 h 0.47697 v 0.47697 c 0,0.32105 0.25825,0.57929 0.57929,0.57929 h 0.34572 c 0.32104,0 0.57929,-0.25824 0.57929,-0.57929 v -0.47697 h 0.47697 c 0.3210503,0 0.5793003,-0.25825 0.5793003,-0.5793 v -0.34571 c 0,-0.32105 -0.25825,-0.57929 -0.5793003,-0.57929 h -0.47697 v -0.47698 c 0,-0.32104 -0.25825,-0.57929 -0.57929,-0.57929 z"/>
-                                            </g>
-                                        </svg>
+                                    <div className='mb-3'>
+                                        <Image src="/agency.jpg" width="64px" height="64px"/>
+                                    </div> 
                                         <span className="text-2xl font-medium uppercase cursor-pointer">Agency</span>
                                     </label>
                                 </div>
@@ -155,26 +151,18 @@ export default function Spsf({mData}) {
                                 <div className={styles.accounttype+" w-full h-[150px] flex flex-row mr-3 mb-3 md:mb-0"}>
                                     <input {...register("accounttype")}  defaultValue={state.accounttype} type="radio" className="btn-check" name="accounttype" value="clinic" id="clinic-01" autoComplete="off" />
                                     <label htmlFor="clinic-01" className="w-full h-[150px] border rounded flex flex-col justify-center items-center transition ease-in">
-                                        <svg className="mb-3" xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 16.933 16.933">
-                                            <g transform="translate(0 -280.067)">
-                                                <path fill="none" stroke="#00003a" strokeLinecap="round" strokeLinejoin="round" strokeWidth=".529" d="M5.855718 280.59582c-1.1447391 0-2.0649578.91071-2.0649578 2.04356v13.83144h9.3518218v-13.83144c0-1.13285-.92022-2.04356-2.064977-2.04356zM2.8399138 285.41743H2.5941292c-1.1447431 0-2.06496273.91067-2.06496273 2.04352v9.00987H3.7907602"/>
-                                                <path fill="none" stroke="#00003a" strokeLinecap="round" strokeLinejoin="round" strokeWidth=".529" d="M14.136491 285.41743h.202722c1.144743 0 2.064963.91067 2.064963 2.04352v9.00987h-3.261594M2.0212258 289.19905H3.79076M2.0212258 291.23698H3.79076M5.5857346 286.89629H7.3552688M5.5857346 288.53332H7.3552688M9.5780732 286.89629H11.347608M9.5780732 288.53332H11.347608M13.142582 289.19905h1.769534M13.142582 291.23698h1.769534M7.7964278 292.25609c-.5170363 0-.9332764.41624-.9332764.93327v3.28146h3.2070396v-3.28146c0-.51703-.4162403-.93327-.9332766-.93327zM5.5857346 290.4042H7.3552688M9.5780732 290.4042H11.347608"/>
-                                                <path fill="none" stroke="#00003a" strokeWidth=".529" d="m 8.2938067,281.99055 c -0.32104,0 -0.57929,0.25825 -0.57929,0.57929 v 0.47698 h -0.47697 c -0.32105,0 -0.5793,0.25824 -0.5793,0.57929 v 0.34571 c 0,0.32105 0.25825,0.5793 0.5793,0.5793 h 0.47697 v 0.47697 c 0,0.32105 0.25825,0.57929 0.57929,0.57929 h 0.34572 c 0.32104,0 0.57929,-0.25824 0.57929,-0.57929 v -0.47697 h 0.47697 c 0.3210503,0 0.5793003,-0.25825 0.5793003,-0.5793 v -0.34571 c 0,-0.32105 -0.25825,-0.57929 -0.5793003,-0.57929 h -0.47697 v -0.47698 c 0,-0.32104 -0.25825,-0.57929 -0.57929,-0.57929 z"/>
-                                            </g>
-                                        </svg>
+                                    <div className='mb-3'>
+                                        <Image src="/clinic.jpg" width="64px" height="64px"/>
+                                    </div> 
                                         <span className="text-2xl font-medium uppercase cursor-pointer">Clinic</span>
                                     </label>
                                 </div>
                                 <div className={styles.accounttype+" w-full h-[150px] flex flex-row mr-3 mb-3 md:mb-0"}>
                                     <input {...register("accounttype")}  defaultValue={state.accounttype} type="radio" className="btn-check" name="accounttype" value="doctor" id="doctor-01" autoComplete="off" />
                                     <label htmlFor="doctor-01" className="w-full h-[150px] border rounded flex flex-col justify-center items-center transition ease-in">
-                                        <svg className="mb-3" xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 16.933 16.933">
-                                            <g transform="translate(0 -280.067)">
-                                                <path fill="none" stroke="#00003a" strokeLinecap="round" strokeLinejoin="round" strokeWidth=".529" d="M5.855718 280.59582c-1.1447391 0-2.0649578.91071-2.0649578 2.04356v13.83144h9.3518218v-13.83144c0-1.13285-.92022-2.04356-2.064977-2.04356zM2.8399138 285.41743H2.5941292c-1.1447431 0-2.06496273.91067-2.06496273 2.04352v9.00987H3.7907602"/>
-                                                <path fill="none" stroke="#00003a" strokeLinecap="round" strokeLinejoin="round" strokeWidth=".529" d="M14.136491 285.41743h.202722c1.144743 0 2.064963.91067 2.064963 2.04352v9.00987h-3.261594M2.0212258 289.19905H3.79076M2.0212258 291.23698H3.79076M5.5857346 286.89629H7.3552688M5.5857346 288.53332H7.3552688M9.5780732 286.89629H11.347608M9.5780732 288.53332H11.347608M13.142582 289.19905h1.769534M13.142582 291.23698h1.769534M7.7964278 292.25609c-.5170363 0-.9332764.41624-.9332764.93327v3.28146h3.2070396v-3.28146c0-.51703-.4162403-.93327-.9332766-.93327zM5.5857346 290.4042H7.3552688M9.5780732 290.4042H11.347608"/>
-                                                <path fill="none" stroke="#00003a" strokeWidth=".529" d="m 8.2938067,281.99055 c -0.32104,0 -0.57929,0.25825 -0.57929,0.57929 v 0.47698 h -0.47697 c -0.32105,0 -0.5793,0.25824 -0.5793,0.57929 v 0.34571 c 0,0.32105 0.25825,0.5793 0.5793,0.5793 h 0.47697 v 0.47697 c 0,0.32105 0.25825,0.57929 0.57929,0.57929 h 0.34572 c 0.32104,0 0.57929,-0.25824 0.57929,-0.57929 v -0.47697 h 0.47697 c 0.3210503,0 0.5793003,-0.25825 0.5793003,-0.5793 v -0.34571 c 0,-0.32105 -0.25825,-0.57929 -0.5793003,-0.57929 h -0.47697 v -0.47698 c 0,-0.32104 -0.25825,-0.57929 -0.57929,-0.57929 z"/>
-                                            </g>
-                                        </svg>
+                                    <div className='mb-3'>
+                                        <Image src="/doctor-icon.svg" width="64px" height="64px"/>
+                                    </div> 
                                         <span className="text-2xl font-medium uppercase cursor-pointer">Doctor</span>
                                     </label>
                                 </div>
